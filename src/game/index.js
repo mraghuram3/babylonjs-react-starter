6538d6717ce4dafb36b703a6d396611916610939
Raghu <raghu@vimals-MacBook-Pro-2.local>
2022-06-13 22:50:47 +0530

    removed usnued code

--- src/game/index.js
+++ src/game/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import {
-    FreeCamera,
+  ArcRotateCamera,
   Vector3,
   HemisphericLight,
   MeshBuilder
@@ -23,23 +23,25 @@ function GameScene() {
 
     const canvas = scene.getEngine().getRenderingCanvas();
 
-    let camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-
-    // This targets the camera to scene origin
-    camera.setTarget(Vector3.Zero());
+    let camera = new ArcRotateCamera(
+      "camera1",
+      -Math.PI / 2,
+      1.2,
+      50,
+      new Vector3(0, 0, 0),
+      scene
+    );
 
     // This attaches the camera to the canvas
     camera.attachControl(canvas, true);
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
     let light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-
     // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+    light.intensity = 0.8;
 
     // Our built-in 'sphere' shape.
     let sphere = MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-
     // Move the sphere upward 1/2 its height
     sphere.position.y = 1;
 
